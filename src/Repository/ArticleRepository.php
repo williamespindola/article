f786276c550755134c2f82e53672b0faeb3d1f29
William Espindola <williamespindola@instinfo.com.br>
2015-06-17 00:20:33 -0300

    Create a Repository interface

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -5,7 +5,9 @@ namespace WilliamEspindola\Article\Repository;
 use WilliamEspindola\Article\Entity\Article;
 use WilliamEspindola\Article\Storage\ORM\StorageORMInterface;
 
-class ArticleRepository extends RepositoryAbstract
+class ArticleRepository 
+    extends RepositoryAbstract
+    implements RepositoryInterface
 {
     public function __construct(StorageORMInterface $storage)
     {
